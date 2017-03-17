2262e9fd38e334f0b81f8d57ef18f7851a9e489e
Kensaigm <scarroll10@student.cccd.edu>
2017-03-16 17:36:38 -0700

    Added Mover.cs updates to Player Prefab.

---
+++ Crossy Road/Assets/Crossy Road/Scripts/Mover.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	public float speed			= 1.0f;
+	public float moveDirection	= 0;
+	public bool parentOnTrigger = true;
+	public bool hitBoxOnTrigger = false;
+	public GameObject moverObject = null;
+
+	private Renderer renderer	= null;
+	private bool isVisible		= false;
+
+	// Use this for initialization
+	void Start () {
+		renderer = moverObject.GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(speed * Time.deltaTime, 0, 0);
+		IsVisible();
+	}
+
+	void IsVisible() {
+		if (renderer.isVisible)
+		{
+			isVisible = true;
+		}
+
+		if ( !renderer.isVisible && isVisible)
+		{
+			Debug.Log(" Remove object. No longer seen by camera.");
+			Destroy(gameObject);
+		}
+	}
+
+	void OnTriggerEnter( Collider other) { }
+	void OnTriggerExit(Collider other) { }
+}
