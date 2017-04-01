f237192925480267405aa26eb6fcf84a8baef198
Kensaigm <scarroll10@student.cccd.edu>
2017-04-01 13:18:14 -0700

    Updated water.prefab, added LevelGenerator.cs

---
+++ Crossy Road/Assets/Crossy Road/Scripts/LevelGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+	public List<GameObject> platform = new List<GameObject>();
+	public List<float> height = new List<float>();
+
+
+
+	private int rndRange = 0;
+	private float lastPos = 0;
+	private float lastScale = 0;
+
+
+	public void RandomGenerator()
+	{
+
+
+
+	}
+
+	public void CreateLevelObject (GameObject obj, float height, int value )
+	{
+
+
+
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
